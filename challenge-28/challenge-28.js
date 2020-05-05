98048f358f7f863abbb7c7f9fd725ce5bc445128
André Rodrigues <dev-asantos@outlook.com>
2020-05-05 11:47:52 -0400

    challenge 28 - Iniciando

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,7 @@
+( function( win, doc ) {
+
+  'use strict';
+
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +29,91 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  // SOLUÇÃO DA AULA 26
+  function DOM( elements ) {
+    this.element = doc.querySelectorAll( elements );
+    }
+
+    DOM.prototype.on = function on( eventType, callback ) {
+    Array.prototype.forEach.call( this.element, function( element ) {
+        element.addEventListener( eventType, callback, false );
+    } );    
+    };
+
+    DOM.prototype.off = function off( eventType, callback ) {
+    Array.prototype.forEach.call( this.element, function( element ) {
+        element.removeEventListener( eventType, callback, false );
+    } );    
+    };
+
+    DOM.prototype.get = function get() {
+    return this.element;
+    };
+    // SOLUÇÃO DA AULA 26
+
+    // SOLUÇÃO DA AULA 27A
+    DOM.prototype.forEach = function forEach() {
+        return Array.prototype.forEach.apply( this.element, arguments );
+    };
+
+    DOM.prototype.map = function map() {
+        return Array.prototype.map.apply( this.element, arguments );
+    };
+
+    DOM.prototype.filter = function filter() {
+        return Array.prototype.filter.apply( this.element, arguments );
+    };
+
+    DOM.prototype.reduce = function reduce() {
+        return Array.prototype.reduce.apply( this.element, arguments );
+    };
+
+    DOM.prototype.reduceRight = function reduceRight() {
+        return Array.prototype.reduceRight.apply( this.element, arguments );
+    };
+
+    DOM.prototype.every = function every() {
+        return Array.prototype.every.apply( this.element, arguments );
+    };
+
+    DOM.prototype.some = function some() {
+        return Array.prototype.some.apply( this.element, arguments );
+    };
+    // SOLUÇÃO DA AULA 27A
+    
+    // SOLUÇÃO DA AULA 27B
+    DOM.isArray = function isArray( param ) {
+        return Object.prototype.toString.call( param ) === '[object Array]';
+    };
+    
+    DOM.isObject = function isObject( param ) {
+        return Object.prototype.toString.call( param ) === '[object Object]';
+    };
+
+    DOM.isFunction = function isFunction( param ) {
+        return Object.prototype.toString.call( param ) === '[object Function]';
+    };
+
+    DOM.isNumber = function isNumber( param ) {
+        return Object.prototype.toString.call( param ) === '[object Number]';
+    };
+
+    DOM.isString = function isString( param ) {
+        return Object.prototype.toString.call( param ) === '[object String]';
+    };
+
+    DOM.isBoolean = function isBoolean( param ) {
+        return Object.prototype.toString.call( param ) === '[object Boolean]';
+    };
+
+    DOM.isNull = function isNull( param ) {
+        return Object.prototype.toString.call( param ) === '[object Null]' || Object.prototype.toString.call( param ) === '[object Undefined]';
+    };
+    // SOLUÇÃO DA AULA 27B
+
+    // SOLUÇÃO DA AULA 28
+    
+    // SOLUÇÃO DA AULA 28
+
+} ) ( window, document );
